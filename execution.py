de15396ffabe943b69cae8d583b7d67f1e0bb4d5
Hakim Mabike <hakimmabike2@gmail.com>
2024-08-31 19:50:06 +0100

    Started execution time project

---
+++ execution.py
@@ -0,0 +1,18 @@
+def timer(func):
+    from time import time
+
+    t1 = time()
+    func
+    t2 = time()
+
+    diff = t2 - t1
+    print(f"Time taken for function: {diff}")
+
+
+@timer
+def prime(num):
+    list = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            list.append(i)
+    return list
