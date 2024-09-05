f93a7e67b97330f6d0e0b53a6453500732e49874
Hakim Mabike <hakimmabike2@gmail.com>
2024-09-05 09:02:09 +0100

    realised the bug: forgot to put the semi-colon on line 29 in connect4.js

--- connect 4/connect4.js
+++ connect 4/connect4.js
@@ -26,7 +26,8 @@ function setGame() {
             tile.id = r.toString() + "-" + c.toString();
             // The code above creates the index of each tile on the board
             // e.g. [2,7] or [0,1]
-            tile.classList.add("tile")
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
             document.getElementById("Board").append(tile);
             // We create a tag like this: <div id="0-0" class="tile"></div>
             // and we append it into our div that contains the id "board"
@@ -34,3 +35,4 @@ function setGame() {
         board.push(row)
     }
 }
+
