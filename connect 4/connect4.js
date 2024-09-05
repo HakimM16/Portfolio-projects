d69907008760d6ee5212e7c0beb5da1f35a9b51a
Hakim Mabike <hakimmabike2@gmail.com>
2024-09-05 12:29:50 +0100

    finished all the functions

--- connect 4/connect4.js
+++ connect 4/connect4.js
@@ -65,4 +65,68 @@ function setPiece() {
 
     r -= 1; // updating the row height for the column
     currColumns[c] = r; // update the array
+
+    checkWinner();
+}
+
+function checkWinner() {
+    // horizontal check
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // vertically
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //anti diagonally
+    for (let r = 0; r < rows-3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // diagonally
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("Winner");
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red wins";
+    } else {
+        winner.innerText = "Yellow Wins";
+    }
+
+    gameOver = True;
 }
