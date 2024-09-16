a0ffcd84472fce189b0fc9e7266a0ebf68d16ac6
Hakim Mabike <hakimmabike2@gmail.com>
2024-09-16 11:09:25 +0100

    Added negative num error

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,3 +1,10 @@
+"""
+This is a fibonacci sequence where it outputs the sequence in a list form and 
+outputs the nth number of the sequence
+"""
+class NegativeNumError(Exception):
+    pass
+
 def generator(num):
     # Outputs a fibonacci sequence at n elements
     fib_array = [0, 1]
@@ -8,18 +15,24 @@ def generator(num):
     return fib_array
 
 def get_nth_num(num):
+    # outputs the nth number of the fibonacci sequence
     sequence = generator(num)
     nth_num = sequence[num - 1]
     return f"F{num}: {nth_num}"
 
 loop = True
 while loop:
+    # Added a try-except block to deal with exceptions
     try:
         number = int(input("Enter the length of the fibonacci sequence: "))
+        if number < 0:
+            raise NegativeNumError("Number can't be negative")
         fib = generator(number)
-        print(f"Fibonacci equence of length {number}: {fib}")
+        print(f"Fibonacci sequence of {number} numbers: {fib}")
         print(f"F{number}: {fib[number - 1]}")
         loop = False
+    except NegativeNumError as e:
+        print(f"Error: {e}")
     except ValueError:
         print("Enter a number!")
     
