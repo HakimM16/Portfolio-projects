b7e59d84aee5064f47a895e87f5472605d20d5ae
Hakim Mabike <hakimmabike2@gmail.com>
2024-09-16 10:59:12 +0100

    Added an input function in a loop

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -7,12 +7,26 @@ def generator(num):
     fib_array.remove(0)
     return fib_array
 
-
 def get_nth_num(num):
     sequence = generator(num)
     nth_num = sequence[num - 1]
     return f"F{num}: {nth_num}"
 
-print(get_nth_num(0))
+loop = True
+while loop:
+    try:
+        number = int(input("Enter the length of the fibonacci sequence: "))
+        fib = generator(number)
+        print(f"Fibonacci equence of length {number}: {fib}")
+        print(f"F{number}: {fib[number - 1]}")
+        loop = False
+    except ValueError:
+        print("Enter a number!")
+    
+    
+
+
+
+
 
 
