7dec8a566157ba0b8852c8b4494fdcbd97979182
Hakim Mabike <hakimmabike2@gmail.com>
2024-09-24 15:21:58 +0100

    done 2

--- longest_palidrome.py
+++ longest_palidrome.py
@@ -2,15 +2,15 @@ def find_longest_palindromic_substring(param):
     longest = "a"
 
     def helper(param):
-        reverse = param[::-1]
-        if reverse == param:
+        reverse = param[::-1].lower()
+        if reverse == param.lower():
             return True
         return False
     
     for i in range(0, len(param)):
         for j in range(i + 1, len(param) + 1):
             substring = param[i:j]
-            print(substring, j)
+            #print(substring, j)
             if helper(substring):
                 if len(substring) > len(longest):
                     longest = substring
@@ -19,7 +19,9 @@ def find_longest_palindromic_substring(param):
     if longest == "a":
         print(f"There is no palidrome in {param}")
     else:
-        print(f"The longest palidrome is {longest}")
+        print(f"The longest palidrome is {longest.lower()}")
 
-#find_longest_palindromic_substring("saippuakivikauppias")
+find_longest_palindromic_substring("saippuakivikauppiasa")
 find_longest_palindromic_substring("abccccdd")
+find_longest_palindromic_substring("Hakim")
+find_longest_palindromic_substring("Racecar")
